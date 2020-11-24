e84fffe216dce317921cc81d1630722e20da0e1c
Hello-CS <mrlamchensian@gmail.com>
2020-11-25 01:01:32 +0800

    Completed. Able to add more features when free

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # etch-a-sketch
+
+Creating a sketchpad
+
+Things Learnt:
+
+1. Grid manipulation
+2. JS control over CSS variables
+3. JS control over HTML
+4. EventListener types
+5. Color randomization
--- index.js
+++ index.js
@@ -1,15 +1,75 @@
 const container = document.querySelector(".container")
-const gridSize= 16 **2
-for (i=0;i<gridSize;i++) {
-    let div = document.createElement('div')
-    div.id ="box"+i;
-    div.classList.add('box')
-    container.appendChild(div)
+const resetButton = document.querySelector('.reset');
+const gridButton = document.querySelector('.grid');
+const rainbowButton = document.querySelector('.rainbow')
+let gridSize = 20
+ gridSizeSquared = 400
+
+cssVar('--gridsize',gridSize);
+createDiv();
+let divAll = document.querySelectorAll('.box')
+        divAll.forEach((div) => {
+            div.addEventListener('mouseover', () => {
+                div.classList.add('highlight')
+        })
+    })
+
+
+//Functions
+function cssVar(rootVar,value) {
+    document.documentElement.style.setProperty(rootVar, value);
 }
 
-const divAll = document.querySelectorAll('.box')
-divAll.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        div.classList.add('highlight')
+function eraseColor() {
+    let divAll = document.querySelectorAll('.box');
+    divAll.forEach((div) => {
+        div.classList.remove('highlight');
     })
-})
+}
+
+function gridSizePrompt() {
+    clearGrid();
+    let input = prompt("Input size of grid");
+    gridSize = Number(input);
+    gridSizeSquared = gridSize**2;
+    cssVar('--gridsize',gridSize);
+    createDiv();
+    let divAll = document.querySelectorAll('.box')
+        divAll.forEach((div) => {
+            div.addEventListener('mouseover', () => {
+                div.classList.add('highlight')
+        })
+    })
+}
+
+function clearGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function createDiv(){
+    for (i=0;i<gridSizeSquared;i++) {
+        let div = document.createElement('div')
+        div.id ="box"+i;
+        div.classList.add('box')
+        container.appendChild(div)
+    }
+}
+
+function turnRainbow() {
+    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16)
+    cssVar('--highlightColor',randomColor)
+}
+
+function turnBlack() {
+    cssVar('--highlightColor','black')
+}
+
+
+
+//buttons
+resetButton.addEventListener('click', eraseColor);
+gridButton.addEventListener('click', gridSizePrompt);
+rainbowButton.addEventListener('click', turnRainbow);
+rainbowButton.addEventListener('dblclick', turnBlack);
